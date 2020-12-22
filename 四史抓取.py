c023f804e2d591f18ea4474eeaf351a09d9eed3c
CooSei <34122355+CooSei@users.noreply.github.com>
2020-12-23 00:51:58 +0800

    Update 四史抓取.py

--- 四史抓取.py
+++ 四史抓取.py
@@ -1,6 +1,6 @@
 import requests
-import json,time,os
-
+import json,time,os,re
+from bs4 import BeautifulSoup
 
 def get_log(my_name):
 	# read log
@@ -12,9 +12,20 @@ def get_log(my_name):
 		scores = []
 		f = open(my_name+'q_a.txt','w')
 		f.close()
-		f = open(my_name+'score.txt','r')
+		f = open(my_name+'score.txt','w')
+		f.close()
+		f = open(my_name+'.txt', 'w')
 		f.close()
 	return q_a,scores
+def decrypt(op):
+	soup = BeautifulSoup(op, 'lxml')
+	soup_text = soup.text
+	# p = re.compile(r'display: ?none;">(.*?)<')
+	p = re.compile((r'display: ?none;(text-decoration:none;)?">(.*?)<'))
+	for i in p.findall(op):
+		if i:
+			soup_text = soup_text.replace(i[1],'')
+	return soup_text
 def get_content(url,my_name,my_index):
 	this_time = str(int(time.time()))
 	headers = {
@@ -28,7 +39,7 @@ def get_content(url,my_name,my_index):
 	'Referer': 'https://ssxx.univs.cn/clientLogin?redirect=%2Fclient%2Fdetail%2F5f71e934bcdbf3a8c3ba5061',
 	'Accept-Encoding': 'gzip, deflate, br',
 	'Accept-Language': 'zh-CN,zh;q=0.9',
-	'Cookie': '_ga=GA1.2.406742327.1606105391;_gid=GA1.2.379454702.1606105391;tgw_l7_route=e05ea26683e7555591184c50c194d59f;_gat=1',
+	'Cookie': "_ga=GA1.2.406742327.1606105391; tgw_l7_route=be2f17e6fbcb3e6c5202ac57e388ad5a; _gid=GA1.2.25532619.1608623537; _gat=1",
 	}
 	response = requests.get(url, headers=headers)
 	ll = json.loads(response.text)
@@ -43,14 +54,14 @@ def get_content(url,my_name,my_index):
 	every_score = 20
 	with open(my_name+'.txt','a') as f:
 		for l in question_id:
-			url3 = 'https://ssxx.univs.cn/cgi-bin/race/question/?t='+this_time+'&activity_id=5f71e934bcdbf3a8c3ba5061&question_id='+l+'&mode_id='+my_index
-			a = requests.get(url3,headers)
+			url3 = 'https://ssxx.univs.cn/cgi-bin/race/question/?t='+this_time+'&activity_id=5f71e934bcdbf3a8c3ba5061&question_id='+l+'&mode_id='+my_index+'&way=1'
+			a = requests.get(url3, headers)
 			all_data = json.loads(a.text)
-			title = all_data['data']['title']
+			title = decrypt(all_data['data']['title'])
 			ops = []
 			ops_id = []
 			for i in range(len(all_data['data']['options'])):
-				ops.append(all_data['data']['options'][i]['title'])
+				ops.append(decrypt(all_data['data']['options'][i]['title']))
 				ops_id.append(all_data['data']['options'][i]['id'])
 			url4 = 'https://ssxx.univs.cn/cgi-bin/race/answer/'
 			if q_a.get(l, 0) != 0:
@@ -68,7 +79,7 @@ def get_content(url,my_name,my_index):
 			answer = answer_json['data']['correct_ids']
 			word_list = []
 			for k in answer:
-				word = ops[ops_id.index(k)]
+				word = decrypt(ops[ops_id.index(k)])
 				word_list.append(word)
 			if q_a.get(l) == 0:
 				q_a[l] = answer
@@ -95,21 +106,21 @@ def write_log():
 	file.close()
 
 
-if '__name__' == '__main__':
-	url = ''
-	# url 为电脑端登录时扫码后get网址
+if __name__ == '__main__':
+	url =""
+	# url 为电脑端登录时扫码后get网址,获取方式readme有说,类似下面这个,关键信息已隐藏
 	#ex:"https://ssxx.univs.cn/cgi-bin/authorize/token/?t=1606104941788&uid=***************&avatar=https:%2F%2Fnode2d-public.hep.com.cn%2Favatar-*****************-1606104941788&activity_id=5f71e934bcdbf3a8c3ba5061"
 	my_name_list = ['英雄','复兴','创新','信念']
-	my_index_list = ['5f71e934bcdbf3a8c3ba51d'+str(i) for i in range(5,9)]
+	my_index_list = ['5f71e934bcdbf3a8c3ba51d'+str(i) for i in range(5, 9)]
 	for i in range(4):
 		my_index = my_index_list[i]
 		my_name = my_name_list[i]
 		test = strit = size_1 = 0
-		max_loop = 500
+		max_loop = 50
 		max_same = 20
 
-		q_a,scores = get_log(my_name)
-				
+		q_a, scores = get_log(my_name)
+
 		while 1:
 			if strit>max_loop:
 				break
@@ -130,4 +141,3 @@ if '__name__' == '__main__':
 				print('something is wrong')
 				time.sleep(10)
 		write_log()
-		# os.system('shutdown -s -t 120')  #完成后关机
